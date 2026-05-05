5d9128593d70ba7bd03b27961d3cbcd7eddad6e7
Olena Shubina <vronskayanice@gmail.com>
2026-05-05 21:34:24 +0000

    successfully rendered Data service output on Details

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,20 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton } from '@ionic/angular/standalone';
+import { MyData } from '../services/my-data';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.page.html',
   styleUrls: ['./details.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBackButton]
 })
-export class DetailsPage implements OnInit {
+export class DetailsPage implements OnInit{
+  savedWord: string = '';
 
-  constructor() { }
+  constructor (private mds: MyData) { }
 
   ngOnInit() {
+    this.loadData();
+  }
+
+  async loadData() {
+    this.savedWord = await this.mds.get('test_kw'); 
+    console.log(this.savedWord); 
   }
 
 }
