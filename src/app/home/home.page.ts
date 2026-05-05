0079f34e356d9327e084eae82787fa693531dca6
Olena Shubina <vronskayanice@gmail.com>
2026-05-05 22:00:00 +0000

    successfully fetching and displaying trending movies on Home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,20 +1,45 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonButton, IonInput } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonButton, IonInput, IonList, IonThumbnail } from '@ionic/angular/standalone';
 import { MyData } from '../services/my-data';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MyHttp } from '../services/my-http';
+import { HttpOptions } from '@capacitor/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonButton, FormsModule, IonInput],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, FormsModule, IonList, IonThumbnail, CommonModule],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  studentNumber: string = 'G00485547';
+  movies: any[] = []; 
+  
+  apiKey: string = '70b2a2787a3fb6f37ef3c1c565cec19d';
+  baseUrl: string = 'https://api.themoviedb.org/3';
   testKeyword: string = '';
 
-  constructor(private router: Router, private mds: MyData) {}
+  constructor(private router: Router, private mds: MyData, private myHttp: MyHttp) {}
+  
+  ngOnInit() {
+    this.loadTrending();
+  }
 
+  async loadTrending() {
+    let options: HttpOptions = {
+      url: this.baseUrl + '/trending/movie/day?api_key=' + this.apiKey
+    };
+    const result = await this.myHttp.get(options);
+    this.movies = result.data.results;
+    
+    console.log("Загруженные фильмы:", this.movies);
+  
+  }
+
+    
+    
   async testStorage() {
     await this.mds.set('test_kw', this.testKeyword); 
     this.router.navigate(['/details']);
