7c5c0784aac23cc1412324a7d8391b903274b117
Olena Shubina <vronskayanice@gmail.com>
2026-05-07 00:21:11 +0000

    Implemented Home and Favourites navigation icons. Added dynamic Add/Remove Favourites button logic

--- src/app/movie-details/movie-details.page.ts
+++ src/app/movie-details/movie-details.page.ts
@@ -1,17 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton, IonListHeader, IonLabel, IonList, IonItem, IonThumbnail } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton, IonListHeader, IonLabel, IonList, IonItem, IonThumbnail, IonIcon } from '@ionic/angular/standalone';
 import { MyData } from '../services/my-data';
 import { MyHttp } from '../services/my-http';
 import { HttpOptions } from '@capacitor/core';
 import { NavController } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { heart, heartOutline, home } from 'ionicons/icons';
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.page.html',
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBackButton, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton, IonListHeader, IonLabel, IonList, IonItem, IonThumbnail]
+  imports: [
+    IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, 
+    IonBackButton, IonCard, IonCardHeader, IonCardTitle, 
+    IonCardContent, IonButton, IonListHeader, IonLabel, 
+    IonList, IonItem, IonThumbnail, IonIcon, CommonModule, FormsModule
+  ]
 })
 export class MovieDetailsPage {
   movie: any = null;
@@ -20,13 +27,30 @@ export class MovieDetailsPage {
   crew: any[] = [];
   showAllCast: boolean = false;
   
-  constructor (private myHttp: MyHttp, private mds: MyData, private navCtrl: NavController) { }
+  constructor (private myHttp: MyHttp, private mds: MyData, private navCtrl: NavController) {
+    addIcons({ home, heart, heartOutline });
+  }
 
   async ionViewWillEnter() {
     const movieId = await this.mds.get('movie_id');
-    
     if (movieId) {
       await this.loadMovieDetails(movieId);
+      await this.checkIfFavourite();
+    }
+  }
+
+  goHome() {
+    this.navCtrl.navigateRoot('/home');
+  }
+
+  goFavourites() {
+    this.navCtrl.navigateForward('/favourites');
+  }
+
+  async checkIfFavourite() {
+    const favourites = await this.mds.get('favourites') || [];
+    if (this.movie) {
+      this.isFavourite = favourites.some((m: any) => m.id === this.movie.id);
     }
   }
 
@@ -47,11 +71,7 @@ export class MovieDetailsPage {
   }
 
   get visibleCast() {
-    if (this.showAllCast) {
-      return this.cast;
-    } else {
-      return this.cast.slice(0, 5);
-    }
+    return this.showAllCast ? this.cast : this.cast.slice(0, 5);
   }
 
   async openPersonDetails(id: number) {
@@ -59,36 +79,21 @@ export class MovieDetailsPage {
     this.navCtrl.navigateForward(['/details']);
   }
 
-
-
   async addToFavourites() {
     if (this.movie !== null) {
-      let favourites = await this.mds.get('favourites');
-      if (favourites === null) {
-        favourites = [];
-      }
-      let isExisting = false;
-      let existingIndex = -1; 
-      for (let i = 0; i < favourites.length; i++) {
-        let currentMovie = favourites[i];
-        if (currentMovie.id === this.movie.id) {
-          isExisting = true;
-          existingIndex = i;
-        }
-      }
-      if (isExisting === false) {
+      let favourites = await this.mds.get('favourites') || [];
+      const index = favourites.findIndex((m: any) => m.id === this.movie.id);
+
+      if (index === -1) {
         favourites.push(this.movie);
-        await this.mds.set('favourites', favourites);
-        this.isFavourite = true; 
+        this.isFavourite = true;
         alert('Movie added to favourites!');
       } else {
-        favourites.splice(existingIndex, 1); 
-        await this.mds.set('favourites', favourites);
-        this.isFavourite = false; 
+        favourites.splice(index, 1);
+        this.isFavourite = false;
         alert('Movie removed from favourites!');
       }
+      await this.mds.set('favourites', favourites);
     }
   }
 }
-
-
