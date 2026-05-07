e8c4ed4eaa81530971373272e957266586a17e62
Olena Shubina <vronskayanice@gmail.com>
2026-05-07 00:44:45 +0000

    finish favourites page and add details button

--- src/app/favourites/favourites.page.ts
+++ src/app/favourites/favourites.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton, IonList, IonItem, IonThumbnail, IonLabel, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton, IonList, IonItem, IonThumbnail, IonLabel, IonButton, IonIcon, IonCard, IonCardContent } from '@ionic/angular/standalone';
 import { MyData } from '../services/my-data';
 import { NavController } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
@@ -12,7 +12,7 @@ import { trash } from 'ionicons/icons';
   templateUrl: './favourites.page.html',
   styleUrls: ['./favourites.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBackButton, IonList, IonItem, IonThumbnail, IonLabel, IonButton, IonIcon]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBackButton, IonList, IonItem, IonThumbnail, IonLabel, IonButton, IonIcon, IonCard, IonCardContent]
 })
 export class FavouritesPage {
   favouriteMovies: any[] = [];
